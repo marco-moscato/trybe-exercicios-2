c5096b44c27d3993a55cf35bed0540c5252ea15f
Marco Moscato <marcomoscatto@gmail.com>
2022-08-20 10:21:36 -0300

    Exercicio 2 ok

--- modulo-1-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-5.2-javascript-trabalhando-com-elementos/script.js
+++ modulo-1-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-5.2-javascript-trabalhando-com-elementos/script.js
@@ -1,2 +1,4 @@
 // Acesse o elemento elementoOndeVoceEsta.
 console.log(document.getElementById('elementoOndeVoceEsta'));
+// Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+document.getElementById('elementoOndeVoceEsta').parentElement.style.background = 'red';
