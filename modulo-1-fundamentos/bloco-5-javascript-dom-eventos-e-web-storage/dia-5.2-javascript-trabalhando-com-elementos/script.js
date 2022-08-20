d36ee96b8a37e47dd00069ade4d7af19a7d4748a
Marco Moscato <marcomoscatto@gmail.com>
2022-08-20 10:31:02 -0300

    Exercicio 4

--- modulo-1-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-5.2-javascript-trabalhando-com-elementos/script.js
+++ modulo-1-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-5.2-javascript-trabalhando-com-elementos/script.js
@@ -1,8 +1,13 @@
 // 1. Acesse o elemento elementoOndeVoceEsta.
-console.log(document.getElementById('elementoOndeVoceEsta'));
+document.getElementById('elementoOndeVoceEsta');
 
 // 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
 document.getElementById('elementoOndeVoceEsta').parentElement.style.background = 'red';
 
 // 3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
 document.getElementById('primeiroFilhoDoFilho').innerText = 'Primeiro filho do filho';
+
+// 4. Acesse o primeiroFilho a partir de pai.
+console.log(document.getElementById('pai').firstElementChild);
+
+
