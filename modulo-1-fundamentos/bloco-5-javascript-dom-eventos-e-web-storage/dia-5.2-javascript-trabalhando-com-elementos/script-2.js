d60b430a7f21ba43f89d05611c9d9a24619672ce
Marco Moscato <marcomoscatto@gmail.com>
2022-08-20 18:46:35 -0300

    Exercicio 2 - cria tag main

--- modulo-1-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-5.2-javascript-trabalhando-com-elementos/script-2.js
+++ modulo-1-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-5.2-javascript-trabalhando-com-elementos/script-2.js
@@ -1,12 +1,32 @@
-// Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body;
-const createTitle = () => {
-  const callBody = document.getElementsByTagName('body')[0];
-  const createElement = document.createElement('h1');
-  createElement.innerText = 'Exercício 5.2 - JavaScript DOM';
-  console.log(callBody);
-  callBody.appendChild(createElement);
+// Função que chama uma tag
+const callTag = (tag) => {
+    return document.getElementsByTagName(tag)[0];
 }
 
+// Função que cria elemento
+const createElement = (elemento) => {
+    return document.createElement(elemento);
+}
+
+// Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body;
+const createTitle = () => {
+//   const callBody = document.getElementsByTagName('body')[0];
+  const createTitle = createElement('h1');
+  createTitle.innerText = 'Exercício 5.2 - JavaScript DOM';
+//   console.log(callBody);
+  callTag('body').appendChild(createTitle);
+}
+
+// 2. Adicione a tag main com a classe main-content como filho da tag body;
+const createMain = () => {
+    const createMain = createElement('main');
+    createMain.className = 'main-content';
+    callTag('body').appendChild(createMain);
+    // console.log(callTag('body'));
+}
 
 // Function calls
-createTitle();
+// window.onload = 
+    createTitle();
+    createMain();
+
