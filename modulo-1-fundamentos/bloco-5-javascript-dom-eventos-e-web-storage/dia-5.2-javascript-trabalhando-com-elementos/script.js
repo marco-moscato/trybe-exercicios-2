9a21c5fbbff6deaad0f6d877222d2d01e6ee6cdc
Marco Moscato <marcomoscatto@gmail.com>
2022-08-20 10:43:55 -0300

    Exercicio 8

--- modulo-1-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-5.2-javascript-trabalhando-com-elementos/script.js
+++ modulo-1-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-5.2-javascript-trabalhando-com-elementos/script.js
@@ -17,4 +17,7 @@ document.getElementById('elementoOndeVoceEsta').previousElementSibling;
 document.getElementById('elementoOndeVoceEsta').nextSibling;
 
 // 7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
-console.log(document.getElementById ('elementoOndeVoceEsta').nextElementSibling);
+document.getElementById ('elementoOndeVoceEsta').nextElementSibling;
+
+// 8. Agora acesse o terceiroFilho a partir de pai.
+console.log(document.getElementById('pai').children[2]);
